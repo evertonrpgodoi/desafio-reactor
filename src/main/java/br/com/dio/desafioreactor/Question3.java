687ba88f4c1b747f7841a61760a7ee9a7ad75c0e
evertonrpgodoi <38073789+evertonrpgodoi@users.noreply.github.com>
2024-10-23 19:44:11 -0300

    resolving Question3

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -8,6 +8,14 @@ public class Question3 {
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
-
+    public Mono<Void> userIsValid(final User user) {
+        return Mono.just(user)
+                .flatMap(u -> {
+                    if (u.password().length() > 8) {
+                        return Mono.empty();
+                    } else {
+                        return Mono.error(new IllegalArgumentException("Invalid user: password must be longer than 8 characters."));
+                    }
+                });
+    }
 }
